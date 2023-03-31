6d7c95f320b2fcb3d6c9cacd6b9620ba0550c730
Nicole <nicole.georgievva@gmail.com>
2023-03-31 15:14:31 +0300

    Test all parse functions

--- app/src/main/java/com/employees/domain/ParseCSV.kt
+++ app/src/main/java/com/employees/domain/ParseCSV.kt
@@ -4,6 +4,11 @@ import com.file.Employee
 import java.time.LocalDate
 import java.time.format.DateTimeParseException
 
+/**
+Returns a list of Employee objects for valid input of the form:
+empId,projectId,dateFrom,dateTo
+1,233,2013-11-01,2014-01-05
+ */
 fun parseCsv(csv: String?): List<Employee?> {
     if (csv == null) {
         return emptyList()
@@ -14,10 +19,18 @@ fun parseCsv(csv: String?): List<Employee?> {
     }
 }
 
+/**
+ * Returns a list of rows from the csv string, excluding the header row.
+ */
 fun extractCsvRows(csv: String): List<String> {
     return csv.lines().drop(1)
 }
 
+/**
+ * Returns an Employee object for a valid row of the form:
+ * empId,projectId,dateFrom,dateTo
+ * 1,233,2013-11-01,2014-01-05
+ */
 fun parseRow(row: String): Employee? {
     val splittedArr = row.split(",").map {
         it.trim()
@@ -31,6 +44,9 @@ fun parseRow(row: String): Employee? {
     return Employee(empId, projectId, dateFrom, dateTo)
 }
 
+/**
+ * Returns a LocalDate object for a valid date string of the form: yyyy-MM-dd
+ */
 fun parseDate(date: String): LocalDate? {
     return try {
         LocalDate.parse(date)
--- app/src/test/java/com/employees/ParseCsv.kt
+++ app/src/test/java/com/employees/ParseCsv.kt
@@ -1,10 +1,14 @@
 package com.employees
 
+import com.employees.domain.extractCsvRows
 import com.employees.domain.parseCsv
+import com.employees.domain.parseDate
+import com.employees.domain.parseRow
 import com.file.Employee
 import io.kotest.core.spec.style.FreeSpec
 import io.kotest.matchers.collections.shouldBeEmpty
 import io.kotest.matchers.collections.shouldContainExactly
+import io.kotest.matchers.shouldBe
 import java.time.LocalDate
 import java.time.Month
 
@@ -14,36 +18,79 @@ import java.time.Month
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ParseCsv : FreeSpec({
-    "parseCsv should return empty list when input is null" {
-        val result = parseCsv(null)
-        result.shouldBeEmpty()
-    }
+    "parse Csv" - {
+        "parseCsv should return empty list when input is null" {
+            val result = parseCsv(null)
+            result.shouldBeEmpty()
+        }
 
-    "parseCsv should return a list of Employee objects for valid input" {
-        val csv = """
+        "parseCsv should return a list of Employee objects for valid input" {
+            val csv = """
             empId,projectId,dateFrom,dateTo
             1,233,2013-11-01,2014-01-05
             2,345,2013-11-01,2014-01-05
         """.trimIndent()
 
-        val expectedResult = listOf(
-            Employee(
-                1, 233, LocalDate.of(2013, Month.NOVEMBER.value, 1),
-                LocalDate.of(2014, Month.JANUARY.value, 5)
-            ),
-            Employee(
-                2, 345, LocalDate.of(2013, Month.NOVEMBER.value, 1),
-                LocalDate.of(2014, Month.JANUARY.value, 5)
+            val expectedResult = listOf(
+                Employee(
+                    1, 233, LocalDate.of(2013, Month.NOVEMBER.value, 1),
+                    LocalDate.of(2014, Month.JANUARY.value, 5)
+                ),
+                Employee(
+                    2, 345, LocalDate.of(2013, Month.NOVEMBER.value, 1),
+                    LocalDate.of(2014, Month.JANUARY.value, 5)
+                )
             )
-        )
 
-        val result = parseCsv(csv)
-        result.shouldContainExactly(expectedResult)
+            val result = parseCsv(csv)
+            result.shouldContainExactly(expectedResult)
+        }
+
+        "parseCsv should handle empty input correctly" {
+            val csv = ""
+            val result = parseCsv(csv)
+            result.shouldBeEmpty()
+        }
     }
 
-    "parseCsv should handle empty input correctly" {
-        val csv = ""
-        val result = parseCsv(csv)
-        result.shouldBeEmpty()
+    "Extract Csv rows" - {
+        "extractCsvRows should return a list of rows from the csv string, excluding the header row" {
+            val csv = """
+            empId,projectId,dateFrom,dateTo
+            1,233,2013-11-01,2014-01-05
+            2,345,2013-11-01,2014-01-05
+        """.trimIndent()
+
+            val expectedResult = listOf(
+                "1,233,2013-11-01,2014-01-05",
+                "2,345,2013-11-01,2014-01-05"
+            )
+
+            val result = extractCsvRows(csv)
+            result.shouldContainExactly(expectedResult)
+        }
+    }
+
+    "Parse Row" - {
+        "parseRow should return an Employee object for a valid row of the form: empId,projectId,dateFrom,dateTo" {
+            val row = "1,233,2013-11-01,2014-01-05"
+            val expectedResult = Employee(
+                1, 233, LocalDate.of(2013, Month.NOVEMBER.value, 1),
+                LocalDate.of(2014, Month.JANUARY.value, 5)
+            )
+
+            val result = parseRow(row)
+            result.shouldBe(expectedResult)
+        }
+    }
+
+    "Parse Date" - {
+        "parseDate should return a LocalDate object for a valid date string of the form: yyyy-MM-dd" {
+            val date = "2013-11-01"
+            val expectedResult = LocalDate.of(2013, Month.NOVEMBER.value, 1)
+
+            val result = parseDate(date)
+            result.shouldBe(expectedResult)
+        }
     }
 })
